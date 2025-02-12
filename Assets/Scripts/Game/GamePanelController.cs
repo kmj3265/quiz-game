4ab0eb37fdfa90432445983ac4b03aed6e92e46d
kmj3265 <163393242+kmj3265@users.noreply.github.com>
2025-02-12 14:16:45 +0900

    Object Pool 구현

--- Assets/Scripts/Game/GamePanelController.cs
+++ Assets/Scripts/Game/GamePanelController.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePanelController : MonoBehaviour
 {
-
-    public void OnClickGameOverButton()
+    [SerializeField] private GameObject quizCardPrefab;            
+    [SerializeField] private Transform quizCardParent;                 // Quiz Card가 표시될 UI Parent
+    
+    private void Start()
     {
-        GameManager.Instance.QuitGame();
+        InitQuizCards();
     }
+
+    private void InitQuizCards()
+    {
+        var firstCardObject = ObjectPool.Instance.GetObject();
+        var secondCardObject = ObjectPool.Instance.GetObject();
+        var thirdCardObject = ObjectPool.Instance.GetObject();
+    }
+    
 }
